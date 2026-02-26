5519f52944c34307335b6e366e111ae01348093c
gabriel34mar <202411432@mondragonmexico.edu.mx>
2026-02-26 09:25:05 -0600

    Update Examen_STM.c

--- Examen_STM.c
+++ Examen_STM.c
@@ -1,47 +1,39 @@
-#include "stm32f4xx.h"
-
-void delay() {
-    for(volatile int i=0; i<100000; i++);
-}
+#include "stm32f10x.h"
 
 // Tabla 7 segmentos HEX 
 const uint8_t hex7seg[16] = 
 {
-    0x3F, // 0
-    0x06, // 1
-    0x5B, // 2
-    0x4F, // 3
-    0x66, // 4
-    0x6D, // 5
-    0x7D, // 6
-    0x07, // 7
-    0x7F, // 8
-    0x6F, // 9
-    0x77, // A
-    0x7C, // B
-    0x39, // C
-    0x5E, // D
-    0x79, // E
-    0x71  // F
+    0x3F,0x06,0x5B,0x4F,
+    0x66,0x6D,0x7D,0x07,
+    0x7F,0x6F,0x77,0x7C,
+    0x39,0x5E,0x79,0x71
 };
 
-int main() {
+void delay() 
+{
+    for(volatile int i=0; i<100000; i++);
+}
 
-    // Habilita reloj 
-    RCC->AHB1ENR |= (1<<0) | (1<<1) | (1<<2);
+int main() 
+{
 
-    // PB1, PB0 entradas
-    GPIOB->MODER &= ~(0xF);
+    // Habilitar reloj
+    RCC->APB2ENR |= (1<<2) | (1<<3) | (1<<4);
 
-    // PC1, PC0 entradas
-    GPIOC->MODER &= ~(0xF);
+    // PA0–PA8 
+    GPIOA->CRL = 0x33333333;  // PA0–PA7
+    GPIOA->CRH = 0x00000003;  // PA8
 
-    // PA0–PA8 salidas
-    GPIOA->MODER |= 0x15555;
+    // PB0, PB1 
+    GPIOB->CRL &= ~(0xFF);
+    GPIOB->CRL |= 0x44;
+
+    // PC0, PC1 
+    GPIOC->CRL &= ~(0xFF);
+    GPIOC->CRL |= 0x44;
 
     while(1) {
 
-        // Leer switches
         uint8_t b3 = (GPIOB->IDR >> 1) & 1;
         uint8_t b2 = (GPIOB->IDR >> 0) & 1;
         uint8_t b1 = (GPIOC->IDR >> 1) & 1;
@@ -49,23 +41,21 @@ int main() {
 
         uint8_t num = (b3<<3) | (b2<<2) | (b1<<1) | b0;
 
-        // Impar
+        // impar
         if(num & 1)
-            GPIOA->ODR |= (1<<0);
+            GPIOA->BSRR = (1<<0);
         else
-            GPIOA->ODR &= ~(1<<0);
+            GPIOA->BRR = (1<<0);
 
-        // Potencia de dos
-        if(num != 0 && (num & (num - 1)) == 0)
-            GPIOA->ODR |= (1<<1);
+        // potencia de 2
+        if(num != 0 && (num & (num-1)) == 0)
+            GPIOA->BSRR = (1<<1);
         else
-            GPIOA->ODR &= ~(1<<1);
+            GPIOA->BRR = (1<<1);
 
-        //Mostrar en 7 segmentos
-        uint8_t seg = hex7seg[num];
-
-        GPIOA->ODR &= ~(0x1FC);          // limpiar
-        GPIOA->ODR |= (seg << 2);        // escribir segmentos
+        // Mostrar en 7 segmentos 
+        GPIOA->ODR &= ~(0x1FC);
+        GPIOA->ODR |= (hex7seg[num] << 2);
 
         delay();
     }
